67c615320e4859ca77908c1de490b71bd96bc124
theodorosx <theo.bouteilles@orange.fr>
2019-11-27 14:13:45 +0100

    ok

--- aircraft.ml
+++ aircraft.ml
@@ -1,13 +1,14 @@
-type aircraft = {id : int; mutable pos : float*float; mutable speed_vector: float*float; mutable neigbors_list: aircraft list; route: (float*float) list};;
+type aircraft = {id : int; mutable pos : float*float; mutable speed_vector: float*float; mutable neighbors: aircraft list; route: (float*float) list};;
 
-let creer_avion position vitesse =
-	incr id_ref;
-	{id = !id_ref; pos = position; vit = vitesse ; voisins = []};;
+let creer_avion id position vitesse =
+	{id = id; pos = position; speed_vector = vitesse ; neighbors = []};;
 
-let add_pos a p1 p2 =
+let add_pos a (p1,p2) =
 	let (x, y) = a.pos in
 	a.pos <- (x +. p1, y +. p2);;
 
-let modif_vit a v1 v2 =
-	a.vit <- (v1, v2);;
+let modif_vit a speed_vect =
+  a.speed_vector <- speed_vect;;
+
+  
 
