22855e3963624ada99c8e00b6f41ba40c08019ae
23euros <ridwane.aissaoui@gmail.com>
2019-11-27 15:53:33 +0100

    Ebauche de simulation

--- simu.ml
+++ simu.ml
@@ -1,1 +1,12 @@
+open Aircraft ;;
 
+let rec update_pos a_list dt =
+  match a_list with
+    [] -> []
+  | tete:queue -> begin
+      Aircraft.move_aircraft tete;
+      update_pos queue dt
+    end;;
+
+let simu a_list dt ending =
+  while !(ending a_list) do update_pos a_list dt done;;
