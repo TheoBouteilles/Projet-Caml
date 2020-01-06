1bcfc7bedc05066be48bdd97d61c90347d35a6be
23euros <ridwane.aissaoui@gmail.com>
2020-01-06 18:45:44 +0100

    kasirien

--- simu.ml
+++ simu.ml
@@ -1,10 +1,13 @@
 
-let rec update_pos a_list dt =
+let rec update_pos a_list dt table =
   match a_list with
     [] -> []
   | tete:queue -> begin
-      Aircraft.move_aircraft tete;
-      update_pos queue dt
+      let x,y = tete.pos in
+      let a=Printf.sprintf "%.3f,%.3f,%.3f" x y tete.heading in
+      table.(tete.id) <- table.(tete.id)::a
+      Aircraft.move tete;
+      update_pos queue dt table
     end;;
 
 let rec ending a_list =
@@ -12,44 +15,40 @@ let rec ending a_list =
     [] -> true
   | tete:queue -> (Aircraft.arrived tete) && (ending queue);;
 
-let add_to_list a_list w_list =
-  let rec add_to_list_rec a_list =
-    match a_list with
-      [] -> []
-    | tete:queue -> begin
-        let line = Aircraft.print tete in
-        List.append w_list line;
-        add_to_list_rec queue;
-      end
-  in
-  add_to_list_rec a_list;;
-
-let write filename lines =
+let write filename table =
   begin
     let file = open_out filename in
-    let rec write_lines l =
-      match l with
-        [] -> []
-      | tete:queue -> begin
-          output_string tete;
-          write_lines queue;
-        end
-    in
-    write_lines lines;
-    close_out file;
+    for i = 0 to Array.length table do
+      begin
+        Printf.fprintf file "%d" i;
+        Printf.fprintf file "0";
+        let rec write_line line=
+          match l with
+            [] -> []
+          | tete:queue -> begin
+              Printf.fprintf file " %s" tete;
+              write_line queue;
+            end
+        in write_line table.(i);
+        Printf.fprintf "\n";
+      end
+   done
+      close_out file;
   end;;
 
 let simu a_list dt filename =
-  let lines = [] in
+  let naircrafts = List.length a_list in
+  let table = Array.make naircrafts [] in
   begin
-    while !(ending a_list) do begin
-      add_to_list a_list lines;
-      Aircraft.algo a_list;
-      update_pos a_list dt;
-    end
+    while !(ending a_list) do
+      begin
+(*        Aircraft.algo a_list;*)
+        update_pos a_list dt table;
+      end;
     done;
-    write filename lines;
+    for i=0 to Array.length table do List.rev table.(i) done;
+    write filename table;
   end;;
 
-
+aircrafts = [(Aircraft.create_aircraft 0 0,0 0. 100. 0,1000)]
 simu aircrafts 10 "results.txt";;
