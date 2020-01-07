f93fb3a00275de1445f10ae97978fd38a60e5f41
Timothee GAVIN <gavinti@D1-206-38-L.eleve.interne.enac>
2020-01-07 11:52:42 +0100

    some bug solved

--- aircraft.ml
+++ aircraft.ml
@@ -52,15 +52,18 @@ let moveAircraft = fun dt aircraft ->
   aircraft.pos <- Vector2D.add pos (Vector2D.mul v dt)
       
 
-let rec discoverNeighbors = fun a1 aircrafts_list ->
-  match aircrafts_list with
+let rec discoverNeighbors = fun a1 env ->
+  match env with
   |[]-> []
-  |a::q -> if a.id != a1.id && getDistanceAircrafts a1 a < _detectionRadius
-    then a::(discoverNeighbors a1 q)
-    else discoverNeighbors a1 q
+  |a::q ->
+      let dist = getDistanceAircrafts a1 a in
+      Printf.printf "A%d - Distance with A%d is %.3f\n" a1.id a.id dist;
+      if a.id != a1.id && dist < _detectionRadius then
+        a::(discoverNeighbors a1 q)
+      else discoverNeighbors a1 q
 
 
-let getMinimalSeparationDistance = fun a1 a2 h1 h2 ->
+let getMinimalSeparationDistance = fun a1 a2 h1 h2 -> (* might be bugged *)
   let p1 = a1.pos in
   let p2 = a2.pos in
   let v1 = Vector2D.mul
@@ -79,11 +82,11 @@ let getMinimalSeparationDistance = fun a1 a2 h1 h2 ->
       (* cas où les avions s'éloignent *)
       getDistanceAircrafts a1 a2 
     else if t <= _conflictDetectionTime then
-      (* cas où le croisement va se faire dans les 3 min *)
+      (* cas où le croisement va se faire dans les 5min *)
       Vector2D.norm (Vector2D.add p (Vector2D.mul v t))
     else
-      (* cas où le croisement à lieu plus tard : distance min = distance à 3 min*)
-      Vector2D.norm (Vector2D.add p (Vector2D.mul v 180.))  
+      (* cas où le croisement à lieu plus tard : distance min = distance à 5 min*)
+      Vector2D.norm (Vector2D.add p (Vector2D.mul v _conflictDetectionTime))  
 
 
 let getHeadings = fun a ->
@@ -105,6 +108,8 @@ let getConflictMatrix = fun a1 a2 ->
       end
     done;
   done;
+  Printf.printf "A%d - Conflict Matrice with A%d :\n  " a1.id a2.id;
+  Array.iter (fun a ->( Array.iter (fun b ->Printf.printf "%b " b) a );  print_newline ()) matrix;
   matrix
 
 
@@ -121,17 +126,17 @@ let getAvailableHeadings = fun a env->
       let mbr = Rectangle.getMostBalancedRectangle list in
       match mbr with
       |None ->
-        begin
-          let (x,y),h = a1.pos, a1.heading in
-          let (x',y'),h' = a2.pos, a2.heading in
-          Printf.printf "A%d - Conflict Matrice with A%d :\n  " a1.id a2.id;
-          Array.iter (fun a ->( Array.iter (fun b ->Printf.printf "%b " b) a );  print_newline ()) matrix;
-          Printf.printf "A%d - No Solution found at (%.3f,%.3f, h=%.3f) in conflict with A%d at (%.3f,%.3f, h=%.3f) : distance between aircraft = %.3f \n"
-            a1.id x y h a2.id x' y' h' (Vector2D.dist a1.pos a2.pos);
-          raise NoSolution;
+          begin
+            (*logs*)
+            let (x,y),h = a1.pos, a1.heading in
+            let (x',y'),h' = a2.pos, a2.heading in
+            Printf.printf "A%d - No Solution found at (%.3f,%.3f, h=%.3f) in conflict with A%d at (%.3f,%.3f, h=%.3f) : distance between aircraft = %.3f \n"
+              a1.id x y h a2.id x' y' h' (Vector2D.dist a1.pos a2.pos);
+            (*/logs*)
+            raise NoSolution;
         end
       |Some (j,i,width,height) ->
-        Math.Intervalle(i, i+height), Math.Intervalle(j, j+width)
+          Math.Intervalle(i, i+height), Math.Intervalle(j, j+width)
   in
 
   let neighbors = discoverNeighbors a env in
@@ -141,27 +146,36 @@ let getAvailableHeadings = fun a env->
                               else let is,_ = getAvailableIndexes a a' in is)
                        neighbors)
   in
+  (*logs*)
+  Printf.printf "A%d - Neighbors = " a.id;
+  List.iter (fun a -> print_int a.id; print_char ' ') neighbors;
+  print_newline ();
   List.iter (fun (Math.Intervalle (i,j)) -> Printf.printf "A%d - i = %d j=%d \n" a.id i j)
-    intervalles;
+    intervalles;  (* raises a warning but missing case never happen *)
+  (* \log *)
   List.fold_right Math.intersection intervalles (Math.Intervalle(0, _n))
 
 
 let getNextHeading = fun a env ->
   match getAvailableHeadings a env with
   | Math.Empty ->
-    begin
-      let (x,y),h = a.pos, a.heading in
-      Printf.printf "A%d - No solution found at (%.3f, %.3f, h=%.3f)\n"
-        a.id x y h;
-      raise NoSolution;
+      begin
+        (*logs*)
+        let (x,y),h = a.pos, a.heading in
+        Printf.printf "A%d - No solution found at (%.3f, %.3f, h=%.3f)\n"
+          a.id x y h;
+        (*/logs*)
+        raise NoSolution;
     end
   | Math.Intervalle(i,j) ->
     let eps = 2. *. _angleMax /. (float _n) in
     let h = a.heading in
     let h1 = Math.normalize (h -. _angleMax +. (float i) *. eps) in
     let h2 = Math.normalize (h -. _angleMax +. (float j) *. eps) in
+    (* logs *)
     Printf.printf "A%d - Can take heading between %.3f - %.3f \n"
       a.id h1 h2;
+    (* /logs *)
     let goalh = Math.degrees (Vector2D.vect_angle (Vector2D.sub a.destination a.pos)) in
     if Math.normalize ( goalh -. h1) <= Math.normalize (h2 -. h1) then goalh
     else if Math.normalize(goalh -. h1) <= Math.normalize (goalh -. h2) then h1
@@ -169,18 +183,15 @@ let getNextHeading = fun a env ->
     
 
 let rec updateEnv env dt table =
-  match env with
-    [] -> ()
-  | tete::queue -> begin
-      setHeading (getNextHeading tete env) tete;
-      let (x,y),h = tete.pos, tete.heading in
-      Printf.printf "A%d - Position (%.3f, %.3f, h=%.3f)\n" tete.id x y h;
-      let a=Printf.sprintf "%.3f,%.3f,%.3f" x y h in
-      table.(tete.id) <- a::table.(tete.id);
-      moveAircraft dt tete;
-      updateEnv queue dt table
-    end;;
-
+  let updateAircraft a env dt table =
+    setHeading (getNextHeading a env) a;
+    let (x,y),h = a.pos, a.heading in
+    Printf.printf "A%d - Position (%.3f, %.3f, h=%.3f)\n" a.id x y h;
+    let p=Printf.sprintf "%.3f,%.3f,%.3f" x y h in
+    table.(a.id) <- p::table.(a.id);
+    moveAircraft dt a;
+  in
+  List.iter (fun a -> updateAircraft a env dt table) env
 
 let arrived a dt =
   (Vector2D.dist a.pos a.destination) < (a.speed *. dt)
